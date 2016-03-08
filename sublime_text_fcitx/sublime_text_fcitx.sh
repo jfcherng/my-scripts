01b7a69d314764ca406206e615abb3dc0e454548
Jack Cherng <jfcherng@gmail.com>
2016-03-08 17:27:39 +0800

    Update input method solution for ST under linux

--- sublime_text_fcitx/sublime_text_fcitx.sh
+++ sublime_text_fcitx/sublime_text_fcitx.sh
@@ -4,8 +4,10 @@
 
 SUBLIME_HOME="/opt/sublime_text"
 
+apt-get update
 apt-get install -y build-essential libgtk2.0-dev
-gcc -Os -shared -o libsublime-imfix.so sublime_imfix.c `pkg-config --libs --cflags gtk+-2.0` -fPIC
+gcc -Os -shared -o libsublime-imfix.so sublime_imfix.c "$(pkg-config --libs --cflags gtk+-2.0)" -fPIC
 mv -f libsublime-imfix.so $SUBLIME_HOME
-cp -f subl `which subl`
+cp -f subl "$(which subl)"
 cp -f sublime_text.desktop /usr/share/applications/
+cp -f sublime_text.desktop $SUBLIME_HOME
