1229ec59bbe63467292fd123e328a8bc5515082d
Jack Cherng <jfcherng@gmail.com>
2016-04-13 13:55:35 +0800

    Update sublime_text_fcitx.sh

--- sublime_text_fcitx/sublime_text_fcitx.sh
+++ sublime_text_fcitx/sublime_text_fcitx.sh
@@ -1,12 +1,18 @@
-#!/bin/bash
-
-# apt-get install -y fcitx fcitx-im
+#!/usr/bin/env bash
 
 SUBLIME_HOME="/opt/sublime_text"
 
+# install input method
+# apt-get install -y fcitx fcitx-im
+
+# install necessary dependencies
 apt-get update
 apt-get install -y build-essential libgtk2.0-dev
-gcc -Os -shared -o libsublime-imfix.so sublime_imfix.c "$(pkg-config --libs --cflags gtk+-2.0)" -fPIC
+
+# compile source codes
+gcc -Os -shared -o libsublime-imfix.so sublime_imfix.c $(pkg-config --libs --cflags gtk+-2.0) -fPIC
+
+# install patches
 mv -f libsublime-imfix.so $SUBLIME_HOME
 cp -f subl "$(which subl)"
 cp -f sublime_text.desktop /usr/share/applications/
