804b755ea51abe219b42a6ace5403e5d7be58c59
Jack Cherng <jfcherng@gmail.com>
2017-09-11 22:01:16 +0800

    Update upgrade_all_pip_packages.sh

--- Ubuntu/upgrade_all_pip_packages.sh
+++ Ubuntu/upgrade_all_pip_packages.sh
@@ -1,3 +1,3 @@
 #/usr/bin/env bash
 
-pip3 freeze --local | command grep -v '^\-e' | cut -d = -f 1  | xargs -n1 sudo pip3 install -U
+pip3 freeze --local | command grep -v '^\-e' | cut -d = -f 1  | xargs -n1 sudo -H pip3 install -U
