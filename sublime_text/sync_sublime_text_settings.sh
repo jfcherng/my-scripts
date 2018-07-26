0b5d87262d07040a34fa526ff8698191316d514b
Jack Cherng <jfcherng@gmail.com>
2018-07-26 18:54:31 +0800

    Update sublime_text/sync_sublime_text_settings.sh
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- sublime_text/sync_sublime_text_settings.sh
+++ sublime_text/sync_sublime_text_settings.sh
@@ -2,8 +2,8 @@
 
 ST_data_dir=~/.config/sublime-text-3
 
-cd "$ST_data_dir" || exit
-git fetch && git reset --hard origin/"$(git rev-parse --abbrev-ref HEAD)"
-git remote update origin --prune
-git gc --prune=now
-cd -
+pushd "$ST_data_dir" || exit
+
+git fetch --all -p && git reset --hard @{upstream}
+
+popd || exit
