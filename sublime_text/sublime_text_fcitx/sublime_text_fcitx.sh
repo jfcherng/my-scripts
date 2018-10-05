2b18ef223827908578386ac563adf6784e7c3e77
Jack Cherng <jfcherng@gmail.com>
2018-10-05 11:53:50 +0800

    Update sublime_text_fcitx
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- sublime_text/sublime_text_fcitx/sublime_text_fcitx.sh
+++ sublime_text/sublime_text_fcitx/sublime_text_fcitx.sh
@@ -1,6 +1,21 @@
 #!/usr/bin/env bash
 
-SUBLIME_HOME=/opt/sublime_text
+ST_EXECUTABLE_FALLBACK="/opt/sublime_text/sublime_text"
+
+# try to find system sublime_text executable
+ST_EXECUTABLE_SYSTEM="$( \
+    command grep -m 1 "^exec" "$(command -v subl)" | \
+    cut -d" " -f2- | \
+    awk -F '"' '{print $1}'
+)"
+
+if [ -f "${ST_EXECUTABLE_SYSTEM}" ]; then
+    ST_EXECUTABLE=${ST_EXECUTABLE_SYSTEM}
+else
+    ST_EXECUTABLE=${ST_EXECUTABLE_FALLBACK}
+fi
+
+ST_HOME="$(dirname "${ST_EXECUTABLE}")"
 
 # install input method
 # sudo apt-get install -y fcitx fcitx-im
@@ -13,7 +28,7 @@ sudo apt install -y build-essential libgtk2.0-dev
 gcc -Os -shared -o "libsublime-imfix.so" "sublime_imfix.c" $(pkg-config --libs --cflags gtk+-2.0) -fPIC
 
 # install patches
-sudo mv -f "libsublime-imfix.so" "${SUBLIME_HOME}"
-sudo cp -f "subl" "$(which subl)"
+sudo mv -f "libsublime-imfix.so" "${ST_HOME}"
+sudo cp -f "subl" "${ST_EXECUTABLE}"
 sudo cp -f "sublime_text.desktop" "/usr/share/applications/"
-sudo cp -f "sublime_text.desktop" "${SUBLIME_HOME}"
+sudo cp -f "sublime_text.desktop" "${ST_HOME}"
