2b4bff89569d0904c7636db60d4df80b231a92bb
Jack Cherng <jfcherng@gmail.com>
2018-10-25 22:40:12 +0800

    Add install_ripgrep.sh
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

---
+++ Ubuntu/install_ripgrep.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+pushd "${SCRIPT_DIR}" || exit
+
+[[ "${UID}" == "0" ]] || { echo "run as sudo to execute"; exit 1; }
+
+apt install -y curl jq
+
+REPO="https://github.com/BurntSushi/ripgrep/releases/download/"
+RG_LATEST=$(curl -sSL "https://api.github.com/repos/BurntSushi/ripgrep/releases/latest" | jq --raw-output .tag_name)
+RELEASE="${RG_LATEST}/ripgrep-${RG_LATEST}-x86_64-unknown-linux-musl.tar.gz"
+
+TMPDIR=$(mktemp -d)
+
+pushd "${TMPDIR}" || exit
+
+wget -O - "${REPO}${RELEASE}" | tar zxf - --strip-component=1
+mv rg /usr/local/bin/
+mv rg.1 /usr/local/share/man/man1/
+mv complete/rg.bash-completion /usr/share/bash-completion/completions/rg
+mandb
+
+popd || exit
+
+rm -rf "${TMPDIR}"
+
+popd || exit
