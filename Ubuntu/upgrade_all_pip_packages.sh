39a0220467db17075c07d00492c9d6e281dcc302
Jack Cherng <jfcherng@gmail.com>
2018-12-19 12:49:00 +0800

    [upgrade_all_pip_packages.sh] Fix sudo under Ubuntu
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- Ubuntu/upgrade_all_pip_packages.sh
+++ Ubuntu/upgrade_all_pip_packages.sh
@@ -2,4 +2,4 @@
 
 [[ "${UID}" == "0" ]] || { echo "run as sudo to execute"; exit 1; }
 
-pip3 freeze --local | command grep -v '^\-e' | cut -d = -f 1 | xargs -n1 -H pip3 install -U
+pip3 freeze --local | command grep -v '^\-e' | cut -d = -f 1 | xargs -n1 pip3 install -U
