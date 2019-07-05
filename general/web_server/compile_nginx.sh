a4834f75b031dd6180e1df8cc4d332da6a093309
Jack Cherng <jfcherng@gmail.com>
2019-07-06 00:29:40 +0800

    [compile_nginx.sh] luajit2/openssl uses git repo
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- general/web_server/compile_nginx.sh
+++ general/web_server/compile_nginx.sh
@@ -10,17 +10,23 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 THREAD_CNT=$(getconf _NPROCESSORS_ONLN)
 NGINX_FLAGS=()
 
+function git_repo_clean {
+    make clean >/dev/null 2>&1
+    git clean -dfx
+    git checkout -- .
+}
+
 
 #--------#
 # config #
 #--------#
 
 NGINX_INSTALL_DIR="/usr/local/nginx"
-LUAJIT2_VERSION="2.1-20190626"
-OPENSSL_VERSION="1.1.1c"
 
+# the command used to clone a repo
 declare -A NGINX_CMD=(
     ["nginx"]="git clone https://github.com/nginx/nginx.git"
+    # modules
     ["ngx_brotli"]="git clone https://github.com/google/ngx_brotli.git ngx_brotli"
     ["ngx_devel_kit"]="git clone https://github.com/simplresty/ngx_devel_kit.git ngx_devel_kit"
     ["ngx_headers_more"]="git clone https://github.com/openresty/headers-more-nginx-module.git ngx_headers_more"
@@ -28,6 +34,18 @@ declare -A NGINX_CMD=(
     ["ngx_http_trim"]="git clone https://github.com/taoyuanyuan/ngx_http_trim_filter_module.git ngx_http_trim"
     ["ngx_lua"]="git clone https://github.com/openresty/lua-nginx-module.git ngx_lua"
     ["ngx_njs"]="git clone https://github.com/nginx/njs.git ngx_njs"
+    # deps
+    ["luajit2"]="git clone https://github.com/openresty/luajit2.git luajit2"
+    ["openssl"]="git clone https://github.com/openssl/openssl.git openssl"
+)
+
+# checkout repo to a specific commit before compilation
+declare -A NGINX_MODULES_CHECKOUT=(
+    # modules
+    ["ngx_njs"]="tags/0.3.3"
+    # deps
+    ["luajit2"]="tags/v2.1-20190626"
+    ["openssl"]="tags/OpenSSL_1_1_1c"
 )
 
 
@@ -54,51 +72,21 @@ for repoName in "${!NGINX_CMD[@]}"; do
 
     pushd "${repoName}" || exit
 
+    git_repo_clean
+
     # fetch the latest source
     git fetch --tags --force --prune --all && git reset --hard "@{upstream}"
     git submodule update --init
     git submodule foreach --recursive git pull
 
+    # checkout a specific commit if required
+    commit=${NGINX_MODULES_CHECKOUT[${repoName}]}
+    if [ "${commit}" != "" ]; then
+        git checkout -f "${commit}"
+    fi
+
     popd || exit
 done
-
-
-#---------------#
-# check luajit2 #
-#---------------#
-
-luajit2_tarball="luajit2-${LUAJIT2_VERSION}.tar.gz"
-luajit2_src_dir="luajit2-${LUAJIT2_VERSION}"
-if [ ! -d "${luajit2_src_dir}" ]; then
-    rm -f -- luajit2-* # also remove downloaded old libs
-    wget --no-check-certificate "https://github.com/openresty/luajit2/archive/v${LUAJIT2_VERSION}.tar.gz" -O "${luajit2_tarball}"
-
-    if [ ! -s "${luajit2_tarball}" ]; then
-        echo "Failed to download luajit2 tarball..."
-        exit 1
-    fi
-
-    tar xf "${luajit2_tarball}"
-fi
-
-
-#---------------#
-# check openssl #
-#---------------#
-
-openssl_tarball="openssl-${OPENSSL_VERSION}.tar.gz"
-openssl_src_dir="openssl-${OPENSSL_VERSION}"
-if [ ! -d "${openssl_src_dir}" ]; then
-    rm -f -- openssl-* # also remove downloaded old libs
-    wget --no-check-certificate "https://www.openssl.org/source/${openssl_tarball}"
-
-    if [ ! -s "${openssl_tarball}" ]; then
-        echo "Failed to download OpenSSL tarball..."
-        exit 1
-    fi
-
-    tar xf "${openssl_tarball}"
-fi
 
 
 #----------------#
@@ -119,12 +107,13 @@ echo "==================================="
 echo "Begin compile 'luajit2'..."
 echo "==================================="
 
-pushd "${luajit2_src_dir}" || exit
+pushd "luajit2" || exit
 
 luajit2_install_dir="/usr/local"
 
 make PREFIX="${luajit2_install_dir}" -j "${THREAD_CNT}" || exit
 make install PREFIX="${luajit2_install_dir}" || exit
+git_repo_clean
 
 ldconfig
 
@@ -150,10 +139,6 @@ echo "==================================="
 
 pushd nginx || exit
 
-# keep in a fresh state
-git clean -dfx
-git checkout -- .
-
 ./auto/configure \
     --prefix=/usr/local/nginx \
     --user=www \
@@ -164,7 +149,7 @@ git checkout -- .
     --with-http_ssl_module \
     --with-http_stub_status_module \
     --with-http_v2_module \
-    --with-openssl="${SCRIPT_DIR}/${openssl_src_dir}" \
+    --with-openssl="${SCRIPT_DIR}/openssl" \
     --add-module="${SCRIPT_DIR}/ngx_brotli" \
     --add-module="${SCRIPT_DIR}/ngx_devel_kit" \
     --add-module="${SCRIPT_DIR}/ngx_headers_more" \
@@ -176,15 +161,7 @@ git checkout -- .
 
 make -j "${THREAD_CNT}" || exit
 make install || exit
-
-
-#----------#
-# clean up #
-#----------#
-
-make clean
-git clean -dfx
-git checkout -- .
+git_repo_clean
 
 popd || exit
 
