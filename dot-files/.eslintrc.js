f5cfb9d5d81be1f5c96d21aecb550f20fda40577
Jack Cherng <jfcherng@gmail.com>
2020-07-08 13:02:28 +0800

    chore: update eslint rules
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

---
+++ dot-files/.eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    es6: true,
+    browser: true,
+    node: true,
+  },
+  // parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  globals: {
+    $: true,
+    jQuery: true,
+  },
+  // extends: ['eslint:recommended', 'eslint-config-prettier'],
+  // plugins: ['prettier'],
+  rules: {
+    // 'prettier/prettier': ['error'],
+    'no-trailing-spaces': ['warn'],
+    'no-unused-vars': ['warn'],
+    semi: ['error'],
+  },
+};
