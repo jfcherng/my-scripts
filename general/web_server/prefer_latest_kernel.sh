3d668caaf23bef7c4fa86da345b21d1246c379b1
Jack Cherng <jfcherng@gmail.com>
2020-10-16 09:52:14 +0800

    feat: add prefer_latest_kernel.sh
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

---
+++ general/web_server/prefer_latest_kernel.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+grep CentOS < "/boot/grub2/grub.cfg"
+
+if [ "$?" = "1" ]; then
+    echo "This script only designed to work on CentOS..."
+    exit 1
+fi
+
+grub2-set-default 0
+grub2-editenv list
+grub2-mkconfig -o "/etc/grub2.cfg"
+
+echo "The system will use the latest kernel as of next boot..."
