4a4a8b01abfa0c213320f6e7d879221d199fd17e
Jack Cherng <jfcherng@gmail.com>
2020-11-02 15:56:28 +0800

    [fix_intelephense.sh] Update policies
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- Ubuntu/fix_intelephense.sh
+++ Ubuntu/fix_intelephense.sh
@@ -1,26 +1,28 @@
 #!/usr/bin/env bash
 
-# Here's an example of activation response from the intelephense server.
-# {"statusCode":200,"data":{"message":{"timestamp":0,"machineId":"YOUR_MACHINE_ID","licenceKey":"YOUR_LICENCE_KEY","expiryTimestamp":999999999,"resultCode":1},"signature":"THE_CALCULATED_SIGNATURE"}}
-
 export FORCE_COLOR=0
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 NOW="$(date +%Y%m%d-%H%M%S)"
 
-NODE_MODULES_LOCATIONS=(
-    # This script's directory
-    "${SCRIPT_DIR}"
+SUB_PATH="intelephense/lib/intelephense.js"
+
+SERVER_BIN_LOCATIONS=(
+    # the first argument
+    "$1"
+    # this script's directory
+    "${SCRIPT_DIR}/$(basename "${SUB_PATH}")"
+    "${SCRIPT_DIR}/node_modules/${SUB_PATH}"
     # ...
-    "$(npm -g root)"
-    "$(yarn global dir)/node_modules"
-    "/usr/local/share/.config/yarn/global"
+    "$(npm -g root 2>nul)/${SUB_PATH}"
+    "$(yarn global dir 2>nul)/node_modules/${SUB_PATH}"
 )
 
-# array unique @see https://stackoverflow.com/questions/13648410
-NODE_MODULES_LOCATIONS=($(printf "%s\n" "${NODE_MODULES_LOCATIONS[@]}" | sort -u))
+VERSION="1.0.1"
+PATCHED_MARKER_DETECTION="/** FILE HAS BEEN PATCHED **/"
+PATCHED_MARKER="${PATCHED_MARKER_DETECTION}/** ${VERSION} **/"
 
-PATCHED_MARKER="// [PATCHED] Intelephense"
+LICENSE_OBJECT_JS='{"message":{"timestamp":0,"machineId":"YOUR_MACHINE_ID","licenceKey":"YOUR_LICENCE_KEY","expiryTimestamp":99999999999,"resultCode":1},"signature":"THE_CALCULATED_SIGNATURE"}'
 
 ##
 ## @brief Change like "C:\Users\..." into "/C/Users/..."
@@ -39,54 +41,65 @@ function windows_path_fix {
     echo "${path}"
 }
 
+
 function patch_intelephense {
     intelephense_js="$1"
 
-    sed -i.bak \
-        $(: license always active ) \
-        -e "s@\bisActive()[[:space:]]*{@\0 return true;@g" \
-        -e "s@\bisExpired()[[:space:]]*{@\0 return false;@g" \
-        -e "s@\bisRevoked()[[:space:]]*{@\0 return false;@g" \
-        $(: enable all capabilities ) \
-        -e "s@\bcodeActionProvider:@\0 true ||@g" \
-        -e "s@\bdeclarationProvider:@\0 true ||@g" \
-        -e "s@\bfoldingRangeProvider:@\0 true ||@g" \
-        -e "s@\bimplementationProvider:@\0 true ||@g" \
-        -e "s@\brenameProvider:@\0 true ||@g" \
-        -e "s@\bselectionRangeProvider:@\0 true ||@g" \
-        -e "s@\btypeDefinitionProvider:@\0 true ||@g" \
-        $(: nullify telemetry ) \
-        -e "s@\bintelephense\.com@does-not-exist.xxx@g" \
-        "${intelephense_js}"
+    # backup the source file
+    file_backup="${intelephense_js}.bak"
+    if [ ! -f "${file_backup}" ]; then
+        cp "${intelephense_js}" "${file_backup}"
+        echo "- Backup '${intelephense_js}' to '${file_backup}'"
+    else
+        echo "- Backup already exists: '${file_backup}'"
+    fi
+
+    search_replace_pairs=(
+        # force convert licenceKey into a non-empty string even if it is undefined
+        "s@(\.initializationOptions\.licenceKey)(?![;)\]}])@\1 + 'FOO_BAR'@g"
+        "s@(\.initializationOptions\.licenceKey)(?=[;)\]}])@\1 = 'FOO_BAR'@g"
+        # license always active
+        "s@\b(activationResult\([^)]*\)[[:space:]]*\{)@\1return this._activationResult = ${LICENSE_OBJECT_JS};@g"
+        "s@\b(readActivationResultFromCache\([^)]*\)[[:space:]]*\{)@\1return this.activationResult = ${LICENSE_OBJECT_JS};@g"
+        # nullify potential telemetry
+        "s@\b(intelephense\.com)@localhost@g"
+    )
+
+    # do patches
+    for pair in "${search_replace_pairs[@]}"; do
+        perl -pi.my_bak -e "${pair}" "${intelephense_js}"
+    done
+    rm -f "${intelephense_js}.my_bak"
 
     # add patched marker
-    echo -e "\n${PATCHED_MARKER}" >> "${intelephense_js}"
+    echo -e "\n\n${PATCHED_MARKER}" >> "${intelephense_js}"
 }
 
-for NODE_MODULES in "${NODE_MODULES_LOCATIONS[@]}"; do
-    NODE_MODULES="$(windows_path_fix "${NODE_MODULES}")"
+for INTELEPHENSE_JS in "${SERVER_BIN_LOCATIONS[@]}"; do
+    if [ "${INTELEPHENSE_JS}" = "" ]; then
+        continue
+    fi
 
-    echo "Test node_modules directory: ${NODE_MODULES}"
+    INTELEPHENSE_JS="$(windows_path_fix "${INTELEPHENSE_JS}")"
 
-    INTELEPHENSE_JS="${NODE_MODULES}/intelephense/lib/intelephense.js"
+    echo "- Test file: ${INTELEPHENSE_JS}"
 
     if [ -f "${INTELEPHENSE_JS}" ]; then
         echo "- Target file: ${INTELEPHENSE_JS}"
 
-        if grep -qF "${PATCHED_MARKER}" "${INTELEPHENSE_JS}"; then
+        if grep -qF "${PATCHED_MARKER_DETECTION}" "${INTELEPHENSE_JS}"; then
             echo "- File seems to be patched already hence skipped."
             continue
         fi
 
-        # backup
-        cp "${INTELEPHENSE_JS}" "${INTELEPHENSE_JS}.${NOW}"
-
         patch_intelephense "${INTELEPHENSE_JS}"
 
         echo "- Patch has been applied."
+
+        break
     fi
 done
 
 echo
-echo 'Done. You may have to fill "licenceKey" with arbitrary text.'
+echo 'Done.'
 echo
