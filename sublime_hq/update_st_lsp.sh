0679c2409d0107e5a528bd004a7153080c107cd4
Jack Cherng <jfcherng@gmail.com>
2021-03-07 20:29:37 +0800

    feat: add update_st_lsp.sh
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

---
+++ sublime_hq/update_st_lsp.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+ST_LSP_PACKAGE_PATH="${APPDATA}\Sublime Text\Installed Packages\LSP.sublime-package"
+INTERESTED_BRANCH="st4000-exploration"
+
+pushd "${SCRIPT_DIR}" || exit
+
+if [[ ! -d "LSP/.git" ]]; then
+    rm -rf "LSP/"
+    git clone "https://github.com/sublimelsp/LSP.git"
+fi
+
+pushd "${SCRIPT_DIR}/LSP" || exit
+
+# fetch latest source
+git fetch --tags --force --prune --all || exit 1
+git reset --hard @{upstream} || exit 1
+git checkout -f "${INTERESTED_BRANCH}" || exit 1
+git pull --autostash --rebase || exit 1
+
+# create package
+git archive HEAD -o out.zip
+7za x -aoa "${ST_LSP_PACKAGE_PATH}" "package-metadata.json"
+7za a -aoa out.zip "package-metadata.json"
+
+# replace ST's package
+mv -f out.zip "${ST_LSP_PACKAGE_PATH}"
+
+git clean -dfx
+
+popd || exit
+
+popd || exit
