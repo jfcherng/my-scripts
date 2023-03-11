83fb83b4ce6a2bf30a54671843cee28a6748c163
Jack Cherng <jfcherng@gmail.com>
2023-03-12 03:07:51 +0800

    [compile_nginx.sh] update deps
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- general/web_server/compile_nginx.sh
+++ general/web_server/compile_nginx.sh
@@ -24,7 +24,7 @@ function git_repo_clean {
 #--------#
 
 NGINX_INSTALL_DIR="/usr/local/nginx"
-OPENSSL_VERSION="3.0.8"
+OPENSSL_VERSION="3.1.4"
 
 # the command used to clone a repo
 declare -A NGINX_CMD=(
@@ -40,7 +40,7 @@ declare -A NGINX_CMD=(
 # checkout repo to a specific commit before compilation
 declare -A NGINX_MODULES_CHECKOUT=(
     # modules
-    ["ngx_njs"]="tags/0.7.10"
+    ["ngx_njs"]="tags/0.8.1"
 )
 
 {
